745b9cbcfe666fac173359ec87967fa171f53c6a
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-10-07 14:14:45 -0300

    cambios / ejemplo

---
+++ talwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // busca clases tailwind en todos los archivos JS/TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
